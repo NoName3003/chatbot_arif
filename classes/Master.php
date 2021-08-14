18b2851fa0a31b3360080b0eeedd95d1e08fae1a
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2021-08-14 17:04:48 +0700

    memahami function
    selanjutnya tinggal bikin ajax, dan ....

--- classes/Master.php
+++ classes/Master.php
@@ -1,99 +1,117 @@
 <?php
 require_once('../config.php');
-Class Master extends DBConnection {
+class Master extends DBConnection
+{
 	private $settings;
-	public function __construct(){
+	public function __construct()
+	{
 		global $_settings;
 		$this->settings = $_settings;
 		parent::__construct();
 	}
-	public function __destruct(){
+	public function __destruct()
+	{
 		parent::__destruct();
 	}
-	
-	public function save_response(){
+
+	public function save_response()
+	{
 		extract($_POST);
-		if(!empty($id)){
+		if (!empty($id)) {
 			$del = $this->conn->query("DELETE FROM `questions` where id= '$id' ");
-			if(!$del){
+			if (!$del) {
 				return 2;
 				exit;
 			}
 		}
 		$data = "";
 		$ins_resp = $this->conn->query("INSERT INTO `responses` set response_message = '{$response_message}' ");
-		if(!$ins_resp){
+		if (!$ins_resp) {
 			return 2;
 			exit;
 		}
 		$resp_id = $this->conn->insert_id;
 
-		foreach($question as $k => $v){
+		foreach ($question as $k => $v) {
 			$data = " response_id = {$resp_id} ";
 			$data .= ", `question` = '$question[$k]' ";
 			$ins[] = $this->conn->query("INSERT INTO `questions` set $data ");
 		}
-		if(isset($ins) && count($ins) == count($question)){
-			$this->settings->set_flashdata("success"," Data successfully saved");
+		if (isset($ins) && count($ins) == count($question)) {
+			$this->settings->set_flashdata("success", " Data successfully saved");
 			return 1;
-		}else{
+		} else {
 			return 2;
 			exit;
 		}
-
 	}
-	public function delete_response(){
+	public function delete_response()
+	{
 		extract($_POST);
-		 $del = $this->conn->query("DELETE FROM `questions` where id = $id");
-		 if($del){
-			$this->settings->set_flashdata("success"," Data successfully deleted");
-		 	return 1;
-		 }else{
-		 	$this->conn->error;
-		 }
+		$del = $this->conn->query("DELETE FROM `questions` where id = $id");
+		if ($del) {
+			$this->settings->set_flashdata("success", " Data successfully deleted");
+			return 1;
+		} else {
+			$this->conn->error;
+		}
 	}
 
-	public function get_response(){
+	public function get_response()
+	{
 		extract($_POST);
 		$message = str_replace(array("?"), '', $message);
-		$not_question = array("what", "what is","who","who is", "where");
-		if(in_array($message,$not_question)){
+		$not_question = array("what", "what is", "who", "who is", "where");
+		if (in_array($message, $not_question)) {
 			$resp['status'] = "success";
 			$resp['message'] = $this->settings->info('no_result');
 			return json_encode($resp);
 			exit;
 		}
-		$sql = "SELECT r.response_message,q.id from `questions` q inner join `responses` r on q.response_id = r.id where q.question Like '%{$message}%' ";
+		$sql = "SELECT r.response_message,q.id,q.question from `questions` q inner join `responses` r on q.response_id = r.id where q.question Like '%{$message}%' ";
+		// $sql = "SELECT id,question from `questions` where question Like  '%{$message}%' ";
 		$qry = $this->conn->query($sql);
-		if($qry->num_rows > 0){
-			$result = $qry->fetch_array();
-			// var_dump($result);
-			$resp['status'] = "success";
-			$resp['message'] = $result['response_message'];
-			$resp['sql'] = $sql;
-			$this->conn->query("INSERT INTO `frequent_asks` set question_id = '{$result['id']}' ");
+
+		// $qry_q = $this->conn->query($sql_q);
+		// if ($qry_q->num_rows > 0) {
+		// 	$result = $qry_q->fetch_array();
+		// 	$resp_q['message'] = $result['response_message'];
+		// 	$resp_q['sql'] = $sql_q;
+		// 	return json_encode($resp_q);
+		// }
+		if ($qry->num_rows > 0) {
+			while ($result = $qry->fetch_array()) {
+				// var_dump($result);
+				$resp['status'] = "success";
+				$resp['message'] = $result['response_message'];
+				$resp['message_q'] = $result['question'];
+				$resp['sql'] = $sql;
+
+				$this->conn->query("INSERT INTO `frequent_asks` set question_id = '{$result['id']}' ");
+			}
 			return json_encode($resp);
-		}else{
+		} else {
 			$resp['status'] = "success";
 			$resp['message'] = $this->settings->info('no_result');
 			$chk = $this->conn->query("SELECT * FROM `unanswered` where `question` = '{$message}' ");
-			if($chk->num_rows > 0){
+			if ($chk->num_rows > 0) {
 				$this->conn->query("UPDATE `unanswered` set no_asks = no_asks + 1 ");
-			}else{
+			} else {
 				$this->conn->query("INSERT INTO `unanswered` set question = '{$message}' ");
 			}
 			return json_encode($resp);
 		}
 	}
-	public function delete_unanswer(){
+	public function delete_unanswer()
+	{
 		extract($_POST);
-		 $del = $this->conn->query("DELETE FROM `unanswered` where id = $id");
-		 if($del){
-			$this->settings->set_flashdata("success"," Data successfully deleted");
-		 	return 1;
-		 }else{
-		 	$this->conn->error;
-		 }
+		$del = $this->conn->query("DELETE FROM `unanswered` where id = $id");
+		if ($del) {
+			$this->settings->set_flashdata("success", " Data successfully deleted");
+			return 1;
+		} else {
+			$this->conn->error;
+		}
 	}
 }
 
@@ -103,16 +121,16 @@ $sysset = new SystemSettings();
 switch ($action) {
 	case 'save_response':
 		echo $Master->save_response();
-	break;
+		break;
 	case 'delete_response':
 		echo $Master->delete_response();
-	break;
+		break;
 	case 'get_response':
 		echo $Master->get_response();
-	break;
+		break;
 	case 'delete_unanswer':
 		echo $Master->delete_unanswer();
-	break;
+		break;
 	default:
 		// echo $sysset->index();
 		break;
--- portal.php
+++ portal.php
@@ -1,11 +1,13 @@
 <style>
-	#chat_convo{
+	#chat_convo {
 		max-height: 65vh;
 	}
-	#chat_convo .direct-chat-messages{
+
+	#chat_convo .direct-chat-messages {
 		min-height: 250px;
 		height: inherit;
 	}
+
 	#chat_convo .card-body {
 		overflow: auto;
 	}
@@ -14,104 +16,130 @@
 	<div class="row">
 		<div class="col-lg-8 <?php echo isMobileDevice() == false ?  "offset-2" : '' ?>">
 			<div class="card direct-chat direct-chat-primary" id="chat_convo">
-              <div class="card-header ui-sortable-handle" style="cursor: move;">
-                <h3 class="card-title">Tanya Pakar</h3>
+				<div class="card-header ui-sortable-handle" style="cursor: move;">
+					<h3 class="card-title">Tanya Pakar</h3>
 
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                </div>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <!-- Conversations are loaded here -->
-                <div class="direct-chat-messages">
-                  <!-- Message. Default to the left -->
-                  <div class="direct-chat-msg mr-4">
-                    <img class="direct-chat-img border-1 border-primary" src="<?php echo validate_image($_settings->info('bot_avatar')) ?>" alt="message user image">
-                    <!-- /.direct-chat-img -->
-                    <div class="direct-chat-text">
-                      <?php echo $_settings->info('intro') ?>
-                    </div>
-                    <!-- /.direct-chat-text -->
-                  </div>
-                  <!-- /.direct-chat-msg -->
+					<div class="card-tools">
+						<button type="button" class="btn btn-tool" data-card-widget="collapse">
+							<i class="fas fa-minus"></i>
+						</button>
+					</div>
+				</div>
+				<!-- /.card-header -->
+				<div class="card-body">
+					<!-- Conversations are loaded here -->
+					<div class="direct-chat-messages">
+						<!-- Message. Default to the left -->
+						<div class="direct-chat-msg mr-4">
+							<img class="direct-chat-img border-1 border-primary" src="<?php echo validate_image($_settings->info('bot_avatar')) ?>" alt="message user image">
+							<!-- /.direct-chat-img -->
+							<div class="direct-chat-text">
+								<?php echo $_settings->info('intro')
+								?>
+								<form id="send_chat_2" method="post">
+									<div class="input-group">
+										<input type="hidden" name="message2" value="bercak" required=""></input>
+										<span class="input-group-append">
+											<button type="submit" class="btn btn-primary">Send</button>
+										</span>
+									</div>
+								</form>
+							</div>
+							<!-- /.direct-chat-text -->
+						</div>
+						<!-- /.direct-chat-msg -->
 
-                  
-                  <!-- /.contacts-list -->
-                </div>
-                <div class="end-convo"></div>
-                <!-- /.direct-chat-pane -->
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer">
-                <form id="send_chat" method="post">
-                  <div class="input-group">
-                    <textarea type="text" name="message" placeholder="Type Message ..." class="form-control" required=""></textarea>
-                    <span class="input-group-append">
-                      <button type="submit" class="btn btn-primary">Send</button>
-                    </span>
-                  </div>
-                </form>
-              </div>
-              <!-- /.card-footer-->
-            </div>
+
+						<!-- /.contacts-list -->
+					</div>
+					<div class="end-convo"></div>
+					<!-- /.direct-chat-pane -->
+				</div>
+				<!-- /.card-body -->
+				<div class="card-footer">
+					<form id="send_chat" method="post">
+						<div class="input-group">
+							<textarea type="text" name="message" placeholder="Type Message ..." class="form-control" required=""></textarea>
+							<span class="input-group-append">
+								<button type="submit" class="btn btn-primary">Send</button>
+							</span>
+						</div>
+					</form>
+					<form id="send_chat" method="post">
+						<div class="input-group">
+							<textarea type="text" name="message" placeholder="Type Message ..." class="form-control" required=""></textarea>
+							<span class="input-group-append">
+								<button type="submit" class="btn btn-primary">Send2</button>
+							</span>
+						</div>
+					</form>
+				</div>
+				<!-- /.card-footer-->
+			</div>
 		</div>
 	</div>
 </div>
 <div class="d-none" id="user_chat">
 	<div class="direct-chat-msg right  ml-4">
-        <img class="direct-chat-img border-1 border-primary" src="<?php echo validate_image($_settings->info('user_avatar')) ?>" alt="message user image">
-        <!-- /.direct-chat-img -->
-        <div class="direct-chat-text"></div>
-        <!-- /.direct-chat-text -->
-    </div>
+		<img class="direct-chat-img border-1 border-primary" src="<?php echo validate_image($_settings->info('user_avatar')) ?>" alt="message user image">
+		<!-- /.direct-chat-img -->
+		<div class="direct-chat-text"></div>
+		<!-- /.direct-chat-text -->
+	</div>
 </div>
 <div class="d-none" id="bot_chat">
 	<div class="direct-chat-msg mr-4">
-        <img class="direct-chat-img border-1 border-primary" src="<?php echo validate_image($_settings->info('bot_avatar')) ?>" alt="message user image">
-        <!-- /.direct-chat-img -->
-        <div class="direct-chat-text"></div>
-        <!-- /.direct-chat-text -->
-  </div>
+		<img class="direct-chat-img border-1 border-primary" src="<?php echo validate_image($_settings->info('bot_avatar')) ?>" alt="message user image">
+		<!-- /.direct-chat-img -->
+		<div class="direct-chat-text"></div>
+		<!-- /.direct-chat-text -->
+	</div>
 </div>
 <script type="text/javascript">
-	$(document).ready(function(){
-		$('[name="message"]').keypress(function(e){
+	$(document).ready(function() {
+		$('[name="message"]').keypress(function(e) {
 			console.log()
-			if(e.which === 13 && e.originalEvent.shiftKey == false){
+			if (e.which === 13 && e.originalEvent.shiftKey == false) {
 				$('#send_chat').submit()
 				return false;
 			}
 		})
-		$('#send_chat').submit(function(e){
+		$('#send_chat').submit(function(e) {
 			e.preventDefault();
 			var message = $('[name="message"]').val();
-			if(message == '' || message == null) return false;
+			if (message == '' || message == null) return false;
 			var uchat = $('#user_chat').clone();
 			uchat.find('.direct-chat-text').html(message);
 			$('#chat_convo .direct-chat-messages').append(uchat.html());
 			$('[name="message"]').val('')
-			$("#chat_convo .card-body").animate({ scrollTop: $("#chat_convo .card-body").prop('scrollHeight') }, "fast");
+			$("#chat_convo .card-body").animate({
+				scrollTop: $("#chat_convo .card-body").prop('scrollHeight')
+			}, "fast");
 
 			$.ajax({
-				url:_base_url_+"classes/Master.php?f=get_response",
-				method:'POST',
-				data:{message:message},
-				error: err=>{
+				url: _base_url_ + "classes/Master.php?f=get_response",
+				method: 'POST',
+				data: {
+					message: message
+				},
+				error: err => {
 					console.log(err)
-					alert_toast("An error occured.",'error');
+					alert_toast("An error occured.", 'error');
 					end_loader();
 				},
-				success:function(resp){
-					if(resp){
+				success: function(resp) {
+
+					if (resp) {
 						resp = JSON.parse(resp)
-						if(resp.status == 'success'){
+						if (resp.status == 'success') {
+							alert(resp.message_q);
 							var bot_chat = $('#bot_chat').clone();
-								bot_chat.find('.direct-chat-text').html(resp.message);
-								$('#chat_convo .direct-chat-messages').append(bot_chat.html());
-								$("#chat_convo .card-body").animate({ scrollTop: $("#chat_convo .card-body").prop('scrollHeight') }, "fast");
+							bot_chat.find('.direct-chat-text').html(resp.message);
+							$('#chat_convo .direct-chat-messages').append(bot_chat.html());
+
+							$("#chat_convo .card-body").animate({
+								scrollTop: $("#chat_convo .card-body").prop('scrollHeight')
+							}, "fast");
 						}
 					}
 				}
